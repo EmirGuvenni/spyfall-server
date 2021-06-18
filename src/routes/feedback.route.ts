6d794aa3eb6b4de924cd21eeb9b142222b916f75
Emir Güvenni <contact@emirguvenni.com>
2021-06-19 01:12:06 +0300

    Upgraded validation

--- src/routes/feedback.route.ts
+++ src/routes/feedback.route.ts
@@ -4,7 +4,14 @@ import { Router, Request, Response } from "express";
 import logger from "../config/logger";
 
 // Import config
-import { DISCORD_AVATAR_URL, DISCORD_EMBED_COLOR } from "../config/constants";
+import {
+	DISCORD_AVATAR_URL,
+	DISCORD_EMBED_COLOR,
+	FEEDBACK_MAX_MESSAGE_LENGTH,
+	FEEDBACK_MAX_NAME_LENGTH,
+	FEEDBACK_MIN_MESSAGE_LENGTH,
+	FEEDBACK_TYPES
+} from "../config/constants";
 
 // Initialize a router
 const router: Router = Router();
@@ -12,16 +19,90 @@ const router: Router = Router();
 router.route('/')
 	.post(postRouteHandler)
 
-function validateEmail(email: string) {
+interface WebhookElements {
+	email: string;
+	name: string;
+	message: string;
+	type: string;
+}
+
+function validateEmail(email: string): boolean {
 	const regex: RegExp = /^(([^<>()[]\.,;:\s@"]+(.[^<>()[]\.,;:\s@"]+)*)|(".+"))@(([[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}])|(([a-zA-Z-0-9]+.)+[a-zA-Z]{2,}))$/;
 	return regex.test(String(email).toLowerCase());
 }
 
+function validateName(name: string): boolean {
+	if (name.length > FEEDBACK_MAX_NAME_LENGTH) return false;
+	else return true;
+}
+
+function validateMessage(message: string): boolean {
+	if (message.length > FEEDBACK_MAX_MESSAGE_LENGTH) return false;
+	else if (message.length < FEEDBACK_MIN_MESSAGE_LENGTH) return false;
+	else return true;
+}
+
+function validateType(type: string): boolean {
+	if (!FEEDBACK_TYPES.includes(type)) return false;
+	else return true;
+}
+
+function validateWebhookElements(elements: WebhookElements) {
+	let missing: string[] = [];
+	let invalid: string[] = [];
+	let isValid: boolean = false;
+
+	// Check if all elements are present and valid
+	if (!elements.email) missing.push('email');
+	else if (!validateEmail(elements.email)) invalid.push('email');
+
+	if (!elements.name) missing.push('name');
+	else if (!validateName(elements.name)) invalid.push('name');
+
+	if (!elements.message) missing.push('message');
+	else if (!validateMessage(elements.message)) invalid.push('message');
+
+	if (!elements.type) missing.push('type');
+	else if (!validateType(elements.type)) invalid.push('type');
+
+
+	missing[0] || invalid[0] ? isValid = false : true;
+
+	return {
+		isValid,
+		missing,
+		invalid
+	}
+}
+
 async function postRouteHandler(req: Request, res: Response): Promise<void> {
-	// Validate the mail address of the incoming feedback
-	if (validateEmail(req.body.email)) {
-		res.send('invalid_mail').status(400).end();
 	let webhookElements: WebhookElements = await req.body;
+	let validation = validateWebhookElements(webhookElements);
+
+	// Send an error if elements are missing or invalid
+	if (!validation.isValid) {
+		// Missing and invalid elements
+		if (validation.missing[0] && validation.invalid[0])
+			res.status(400).json({
+				status: 'error',
+				message: 'There are missing and invalid elements.',
+				missing: validation.missing,
+				invalid: validation.invalid
+			}).end();
+		// Missing elements
+		else if (validation.missing[0])
+			res.status(400).json({
+				status: 'error',
+				message: 'There are missing elements.',
+				missing: validation.missing
+			}).end();
+		// Invalid elements
+		else if (validation.invalid[0])
+			res.status(400).json({
+				status: 'error',
+				message: 'There are invalid elements',
+				invalid: validation.invalid
+			}).end();
 		return;
 	}
 
