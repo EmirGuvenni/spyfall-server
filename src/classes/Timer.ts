ee7fd4ce5832311f5b5d1edae9ec230f56047b14
Emir Güvenni <contact@emirguvenni.com>
2021-06-24 07:38:47 +0300

    Removed the use of roomID
    Removed the roomID because it will be stored in the Room object.

--- src/classes/Timer.ts
+++ src/classes/Timer.ts
@@ -2,29 +2,27 @@
 import { TypedEmitter } from "tiny-typed-emitter";
 
 interface TimerEvents {
-	start: (roomID: string) => void;
-	end: (roomID: string) => void;
+	start: () => void;
+	end: () => void;
 }
 
 export default class Timer extends TypedEmitter<TimerEvents> {
 	public seconds: number;
-	public roomID: string;
 
-	constructor(seconds: number, id: string) {
+	constructor(seconds: number) {
 		super();
 
 		this.seconds = seconds;
-		this.roomID = id;
 	}
 
 	public start = () => {
-		this.emit('start', (this.roomID));
+		this.emit('start');
 
 		setInterval(() => {
 			this.seconds--;
 
 			if (this.seconds === 0)
-				this.emit('end', (this.roomID));
+				this.emit('end');
 		}, 1000);
 	}
 }
