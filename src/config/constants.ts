a8724fd7e1207123f73fa145ae5e2656b86e8d47
Emir Güvenni <contact@emirguvenni.com>
2021-06-24 07:39:16 +0300

    Moved room id chars to the generator function

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -48,7 +48,6 @@ export const WEBSITE_URL: string = 'https://';
  */
 export const ROOM_ID_LENGTH: number = 6;
 export const MAX_ROOM_NAME_LENGTH: number = 12;
-export const ROOM_ID_CHARS: string = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
 
 export const MIN_TIMER_LIMIT: number = 60;
 export const MAX_TIMER_LIMIT: number = 3600;
